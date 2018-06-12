696dd34a0c89f36575b9ef6d660314d283363a6c
Parijat chandra <parijatchandra@parijats-mbp.dhcp.db.de>
2018-06-12 09:48:15 +0530

    Initial Commit

---
+++ CoreML1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML1
+//
+//  Created by Parijat chandra on 12/06/18.
+//  Copyright © 2018 Parijat chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
