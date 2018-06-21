585f565c9d53445102a0564ea0a53bb61ef37dfe
parijat <parijat@Parijats-MacBook-Pro.local>
2018-06-21 11:21:47 +0530

    added 1 line

--- CoreML1/ViewController.swift
+++ CoreML1/ViewController.swift
@@ -7,19 +7,92 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classifier: UILabel!
+    var model = RiceSoupClassifier()//Inceptionv3()//MobileNet() GoogLeNetPlaces()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        model = RiceSoupClassifier()//Inceptionv3()//MobileNet() GoogLeNetPlaces()
+    }
+    @IBAction func camera(_ sender: Any) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        else
+        {
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            cameraPicker.sourceType = .camera
+            present(cameraPicker, animated: true)
+        }
+    }
+    @IBAction func openLibrary(_ sender: Any) {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        cameraPicker.sourceType = .photoLibrary
+        present(cameraPicker, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+}
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true)
+        classifier.text = "Analyzing Image..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 227, height: 227), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 227, height: 227))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+        
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+            return
+        }
+        
+//        guard let prediction = try? VNCoreMLModel(for: GoogLeNetPlaces().model) else {
+//            fatalError("can't load Places ML model")
+//        }
+        classifier.text = "I think this is a \(prediction.foodType)."
+    }
 }
 
